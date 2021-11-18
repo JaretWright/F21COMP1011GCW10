8cd06b7865dda5fb5c16edc80cc58c90a424bf31
JaretWright <jaret.wright@georgiancollege.ca>
2021-11-18 10:51:28 -0500

    Added a listener for the poster art

--- src/main/java/com/example/f21comp1011gcw10/Controllers/SearchViewController.java
+++ src/main/java/com/example/f21comp1011gcw10/Controllers/SearchViewController.java
@@ -1,14 +1,22 @@
 package com.example.f21comp1011gcw10.Controllers;
 
+import com.example.f21comp1011gcw10.APIUtility;
+import com.example.f21comp1011gcw10.ApiResponse;
 import com.example.f21comp1011gcw10.Movie;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class SearchViewController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearchViewController implements Initializable {
 
     @FXML
     private TextField searchTextField;
@@ -26,8 +34,26 @@ public class SearchViewController {
     private Button getDetailsButton;
 
     @FXML
-    private void getSearchResults()
-    {
+    private void getSearchResults() throws IOException, InterruptedException {
+        initialMovieDataListView.getItems().clear();
+        ApiResponse apiResponse = APIUtility.getMoviesFromOMDB(searchTextField.getText());
+        if (apiResponse != null)
+        {
+            initialMovieDataListView.getItems().addAll(apiResponse.getSearch());
+        }
+    }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initialMovieDataListView.getSelectionModel().selectedItemProperty().addListener(
+                (obs, oldMovie, movieSelected) -> {
+                    try{
+                        posterImageView.setImage(new Image(movieSelected.getPoster()));
+                    }catch(Exception e)
+                    {
+                        
+                    }
+
+                });
     }
 }
